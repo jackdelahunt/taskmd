73ab38e9dea70697ef35a5e7ea8f270c6b70aa39
jackdelahunt <jdealhunt@gmail.com>
2024-08-16 11:50:58 +0100

    feat: added default on params

--- main.go
+++ main.go
@@ -196,10 +196,24 @@ func generateInputs(task *pipepinev1.Task, stringBuilder *strings.Builder) error
 	}
 
 	for _, param := range task.Spec.Params {
-		_, err := stringBuilder.WriteString(fmt.Sprintf("* **%v**: %v\n", param.Name, param.Description))
-		if err != nil {
-			return err
+
+		if param.Default != nil {
+			paramString, err := stringifyParam(param.Default)
+			if err != nil {
+				return err
+			}
+
+			_, err = stringBuilder.WriteString(fmt.Sprintf("* **%v**: %v `(Default: %v)`\n", param.Name, param.Description, paramString))
+			if err != nil {
+				return err
+			}
+		} else {
+			_, err := stringBuilder.WriteString(fmt.Sprintf("* **%v**: %v\n", param.Name, param.Description))
+			if err != nil {
+				return err
+			}
 		}
+
 	}
 
 	_, err = stringBuilder.WriteString("\n")
@@ -231,4 +245,45 @@ func generateHeader(task *pipepinev1.Task, stringBuilder *strings.Builder) error
 	}
 
 	return nil
+}
+
+func stringifyParam(param *pipepinev1.ParamValue) (string, error) {
+	if param == nil {
+		return "", fmt.Errorf("cannot stringify nil param value")
+	}
+
+	switch param.Type {
+	case "string":
+		return param.StringVal, nil
+	case "array":
+		var stringBuilder strings.Builder
+		_, err := stringBuilder.WriteString("[")
+		if err != nil {
+			return "", err
+		}
+
+		var formatString string
+
+		for i, s := range param.ArrayVal {
+			if i == len(param.ArrayVal)-1 {
+				formatString = "%v, "
+			} else {
+				formatString = "%v"
+			}
+
+			_, err := stringBuilder.WriteString(fmt.Sprintf(formatString, s))
+			if err != nil {
+				return "", err
+			}
+		}
+
+		_, err = stringBuilder.WriteString("]")
+		if err != nil {
+			return "", err
+		}
+	case "object":
+		return "{}", nil
+	}
+
+	return "", nil
 }
